42c91ece1d1bed8e671805d6a32039dd4eb3d072
Rushabh <rush00121@gmail.com>
2017-01-02 14:30:03 -0800

    added fit_generator , changed network structure

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import argparse
 
 import cv2
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -42,17 +43,17 @@ def normalizeImage(image):
     return image
 
 
-def getnormalizeddata(filelocations, debug=False):
+def getnormalizeddata(filelocations,yoriginaldata, debug=False):
     xnormalized = []
     for filename in filelocations:
         image = getImage(filename)
-        resize = resizeimage(image, 8)
+        resize = resizeimage(image, 4)
         normalizedimage = normalizeImage(resize)
         xnormalized.append(normalizedimage)
         if debug and len(xnormalized) % 100 == 0:
             print("Loaded " + str(len(xnormalized)) + "images")
 
-    ydata = yoriginal
+    ydata = yoriginaldata
     return xnormalized, ydata
 
 
@@ -77,51 +78,77 @@ def augmentdata(xnormalizeddata, ydata):
     return xnormalizeddata, ydata
 
 
-def getFinalData(imagefiles, augment=True):
-    xnormalized, ydata = getnormalizeddata(imagefiles, debug=True)
+def getFinalData(imagefiles,yoriginaldata, augment=False):
+    xnormalized, ydata = getnormalizeddata(imagefiles,yoriginaldata, debug=True)
     if (augment):
         xfinal, yfinal = augmentdata(xnormalized, ydata)
     else:
         xfinal = xnormalized
         yfinal = ydata
-    return np.asarray(xfinal), yfinal
+    return np.asarray(xfinal), np.asarray(yfinal)
 
 
-def generate_data(xdata,ydata,batch_size=32):
+def generate_data(xdata,ydata,batch_size=32,pr_threshold=0.5):
+    batch_x_data = []
+    batch_y_data = []
     while 1:
+        batch_x_data = []
+        batch_y_data = []
+        for i in range(batch_size):
+            rand_i = np.random.randint(len(xdata))
+            keep_pr = 0
+            while keep_pr==0:
+                rand_i = np.random.randint(len(xdata))
+                if abs(ydata[rand_i]) < .1:
+                    pr_val = np.random.uniform()
+                    if(pr_val>pr_threshold):
+                        keep_pr = 1
+                else:
+                    keep_pr = 1
+
+            batch_x_data.append(xdata[rand_i])
+            batch_y_data.append(ydata[rand_i])
+        yield np.asarray(batch_x_data),np.asarray(batch_y_data)
 
 
-        yield xdata,ydata
 
-
-
-def trainmodel(X_train, X_valid, y_train, y_valid, batch_size=128, nb_epoch=20, kernel_size=(5, 5), nb_filters=32,model=None):
+def trainmodel(X_final,Y_final, batch_size=128, nb_epoch=20, kernel_size=(5, 5), nb_filters=32,model=None):
     if model is None:
         model = Sequential()
         model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
                                 border_mode='same',
-                                input_shape=(40, 20, 3,)))
+                                input_shape=(80, 40, 3,)))
         model.add(MaxPooling2D())
+        model.add(Dropout(0.5))
         model.add(Convolution2D(nb_filters * 2, kernel_size[0], kernel_size[1],
                                 border_mode='same',
-                                input_shape=(20, 10, 32,)))
-        model.add(MaxPooling2D())
+                                input_shape=(40, 20, 32,)))
+        #model.add(MaxPooling2D())
+        model.add(Dropout(0.5))
         model.add(Convolution2D(nb_filters * 4, kernel_size[0], kernel_size[1],
                                 border_mode='same',
-                                input_shape=(20, 10, 64,)))
+                                input_shape=(40, 20, 64,)))
         model.add(MaxPooling2D())
+        model.add(Dropout(0.5))
         model.add(Convolution2D(nb_filters * 8, kernel_size[0], kernel_size[1],
                                 border_mode='same',
-                                input_shape=(10, 5, 64,)))
+                                input_shape=(20, 10, 128,)))
+        model.add(MaxPooling2D())
         model.add(Dropout(0.5))
+        model.add(Convolution2D(nb_filters * 16, kernel_size[0], kernel_size[1],
+                                border_mode='same',
+                                input_shape=(10, 5, 256,)))
         model.add(Flatten())
+        model.add(Dense(128))
+        model.add(Dense(64))
+        model.add(Dense(64))
         model.add(Dense(1))
         model.compile(loss='mean_squared_error', optimizer='adam')
     else:
         print("Loaded model from file")
 
     print(model.summary())
-    history = model.fit(X_train, y_train, validation_data=(X_valid, y_valid), batch_size=batch_size, nb_epoch=nb_epoch)
+    history = model.fit_generator(generate_data(X_final,Y_final),batch_size,nb_epoch)
     return model
 
 
@@ -136,26 +163,30 @@ def save_model(model, filename="model.h5",model_arch="model.json"):
 
 
 if __name__ == '__main__':
-    dataframe = pd.read_csv("driving_log.csv", delim_whitespace=False, header=None)
+    dataframe = pd.read_csv("udacity_data/data/driving_log.csv", delim_whitespace=False, header=None)
     centerimages = (dataframe[:][0])
     yoriginal = (dataframe[:][3])
-    xfinal, yfinal = getFinalData(centerimages)
+    xfinal, yfinal = getFinalData(centerimages,yoriginal)
 
-    X_train, X_valid, y_train, y_valid = train_test_split(
-        xfinal,
-        yfinal,
-        test_size=0.05,
-        random_state=832289)
+    # xgen,ygen = next(generate_data(xfinal,yfinal))
+    # print(ygen)
+
+    # X_train, X_valid, y_train, y_valid = train_test_split(
+    #     xfinal,
+    #     yfinal,
+    #     test_size=0.05,
+    #     random_state=832289)
 
     model = None
-    with open('model.json', 'r') as jfile:
-        if jfile is not None:
-            model = model_from_json(json.load(jfile))
-            model.compile("adam", "mse")
-            weights_file = "model.h5"
-            model.load_weights(weights_file)
-            print("Loading model from file")
+    if os.path.isfile("model.json"):
+        with open('model.json', 'r') as jfile:
+            if jfile is not None:
+                model = model_from_json(json.load(jfile))
+                model.compile("adam", "mse")
+                weights_file = "model.h5"
+                model.load_weights(weights_file)
+                print("Loading model from file")
 
-    model = trainmodel(X_train, X_valid, y_train, y_valid, nb_epoch=10,model=model)
+    model = trainmodel(xfinal, yfinal, batch_size=12800,nb_epoch=5,model=model)
     save_model(model)
 
